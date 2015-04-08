2b901b62ec12582920b9b26c8b29cfb194043fa8
Robbbert <pac0446@bigpond.net.au>
2015-04-08 22:06:34 +1000

    opengl always available

--- src/osd/winui/properties.c
+++ src/osd/winui/properties.c
@@ -253,9 +253,7 @@ static struct ComboBoxVideo
 	{ TEXT("DirectDraw"),           "ddraw"   },
 	{ TEXT("Direct3D"),             "d3d"     },
 //	{ TEXT("BGFX"),                 "bgfx"    },
-#if (USE_OPENGL)
 	{ TEXT("OpenGL"),               "opengl"  },
-#endif
 };
 #define NUMVIDEO ARRAY_LENGTH(g_ComboBoxVideo)
 
