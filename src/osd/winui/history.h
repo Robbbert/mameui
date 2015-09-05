fbdc8afeb88d14dd754bd2e8b1861cd0be1416a9
Robbbert <pac0446@bigpond.net.au>
2015-09-06 00:55:07 +1000

    Enhanced the history tab.

--- src/osd/winui/history.h
+++ src/osd/winui/history.h
@@ -1,10 +1,21 @@
-// For licensing and usage information, read docs/winui_license.txt
-//****************************************************************************
+/***************************************************************************
 
+  M.A.M.E.UI  -  Multiple Arcade Machine Emulator with User Interface
+  Win32 Portions Copyright (C) 1997-2003 Michael Soderstrom and Chris Kirmse,
+  Copyright (C) 2003-2007 Chris Kirmse and the MAME32/MAMEUI team.
+
+  This file is part of MAMEUI, and may only be used, modified and
+  distributed under the terms of the MAME license, in "readme.txt".
+  By continuing to use, modify or distribute this file you indicate
+  that you have read the license and understand and accept it fully.
+
+ ***************************************************************************/
+
+#pragma once 
+ 
 #ifndef HISTORY_H
 #define HISTORY_H
 
 char * GetGameHistory(int driver_index);
 
 #endif
-
