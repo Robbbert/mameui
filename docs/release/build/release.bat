65f3006da4de1f10f924ffbf6d419d089abedd3c
Robbbert <Robbbert@users.noreply.github.com>
2016-10-26 15:58:30 +1100

    0.179 Release files

--- docs/release/build/release.bat
+++ docs/release/build/release.bat
@@ -1,3 +1,8 @@
+@echo off
+echo HAVE YOU UPDATED version.cpp ???
+pause
+
+
 call newsrc.bat
 call clean.bat
 call clean.bat
@@ -7,5 +12,10 @@ call clean.bat
 rem --- 32bit ---
 del mameui.exe
 call make32 -j4 %1 %2 %3
+if not exist mameui.exe goto end
+
+rem --- 64bit ---
+del mameui64.exe
+call make64 -j4 %1 %2 %3
 
 :end
