327b971dd84f8ff877143ce5de5f9ce4afd61d7e
Robbbert <Robbbert@users.noreply.github.com>
2023-06-22 22:39:46 +1000

    fix version

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 
 ***************************************************************************/
 
-#define BARE_BUILD_VERSION "0.255.0"
+#define BARE_BUILD_VERSION "0.255.x"
 
 extern const char bare_build_version[];
 extern const char build_version[];
