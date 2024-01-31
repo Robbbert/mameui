2300aeb76ebd77eda237c7feb7b4e37fc245a019
Robbbert <Robbbert@users.noreply.github.com>
2024-01-31 19:45:21 +1100

    Files for 262.

--- docs/release/build/uprel.bat
+++ docs/release/build/uprel.bat
@@ -1,4 +1,4 @@
-goto end
+\goto end
 git fetch upstream
 git merge upstream/release0262
 git checkout master
--- docs/release/src/version.cpp
+++ docs/release/src/version.cpp
@@ -8,7 +8,7 @@
 
 ***************************************************************************/
 
-#define BARE_BUILD_VERSION "0.261.2"
+#define BARE_BUILD_VERSION "0.262.0"
 
 extern const char bare_build_version[];
 extern const char build_version[];
